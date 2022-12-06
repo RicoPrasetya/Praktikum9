ef87c879bf0548706a68208dfa0b4c07c159e43f
Rico Prasetya <ricoprasetya24@gmail.com>
2022-12-06 10:53:38 +0700

    menambah tugas

--- Tugas/latihan1.py
+++ Tugas/latihan1.py
@@ -1,1 +1,10 @@
-“# Latihan1”
+print("--Class--")
+print("")
+class person():
+    def __init__(self,nama,):
+        self.nama=nama
+        self.alamat=""
+        ari = person('Ari')
+        dina = person('Dina')
+    def cetak(self):
+    print person(self.nama)
